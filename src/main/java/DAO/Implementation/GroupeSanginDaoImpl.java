ea4ffbe1055171d5b3a0ecef00fc324b4c7aaa9a
mejdaoui.soufiane <soufiane.mejdaoui@um5s.net.ma>
2020-01-21 01:58:21 +0100

    GroupSanginDAO DONE!

--- src/main/java/DAO/Implementation/GroupeSanginDaoImpl.java
+++ src/main/java/DAO/Implementation/GroupeSanginDaoImpl.java
@@ -2,11 +2,56 @@ package DAO.Implementation;
 
 import DAO.DAOFactory;
 import DAO.interfaces.GroupeSanginDAO;
+import Model.GroupeSangin;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GroupeSanginDaoImpl implements GroupeSanginDAO {
     private DAOFactory daoFactory;
 
     public GroupeSanginDaoImpl(DAOFactory daoFactory) {
         this.daoFactory = daoFactory;
+    }
+
+
+    @Override
+    public GroupeSangin findGroupSanginByName(String nom) {
+        String query = "select * from GroupeSangin where nomGS = '"+nom+"'";
+        try {
+            Connection connection = daoFactory.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            if(resultSet.next()){
+                GroupeSangin groupeSangin = new GroupeSangin(resultSet.getInt("idGS"),resultSet.getString("nomGS"));
+                return  groupeSangin;
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public List<GroupeSangin> findAll() {
+        String query = "SELECT * FROM GroupeSangin";
+        try {
+            Connection connection = daoFactory.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            List<GroupeSangin> groupeSangins = new ArrayList<>();
+            while(resultSet.next()){
+                GroupeSangin groupeSangin = new GroupeSangin(resultSet.getInt("idGS"),resultSet.getString("nomGS"));
+                groupeSangins.add(groupeSangin);
+            }
+            return groupeSangins;
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
--- src/main/java/DAO/interfaces/GroupeSanginDAO.java
+++ src/main/java/DAO/interfaces/GroupeSanginDAO.java
@@ -1,4 +1,13 @@
 package DAO.interfaces;
 
+import Model.GroupeSangin;
+
+import java.util.List;
+
 public interface GroupeSanginDAO {
+
+    //public GroupeSangin findGroupSanginById(int id);
+    public GroupeSangin findGroupSanginByName(String name);
+    public List<GroupeSangin> findAll();
+
 }
