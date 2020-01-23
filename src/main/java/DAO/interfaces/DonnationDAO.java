21cfbec7a989fd16d2915cd8d15b5bb2d2c3e83d
OsmMossati <56207208+OsmMossati@users.noreply.github.com>
2020-01-23 01:53:40 +0100

    Donnation DAO & Impl Part1

--- src/main/java/DAO/interfaces/DonnationDAO.java
+++ src/main/java/DAO/interfaces/DonnationDAO.java
@@ -1,4 +1,23 @@
 package DAO.interfaces;
 
+import Model.Donnation;
+
+import java.sql.Timestamp;
+import java.util.List;
+
 public interface DonnationDAO {
+
+    boolean addDonnation(Donnation donnation);
+    boolean updateDonnation(int idDonnateur, Timestamp dateDonnation, Donnation donnation);
+    boolean deleteDonnation(int idDonnateur, Timestamp dateDonnation);
+
+    List<Donnation> getAllDennation();
+    List<Donnation> getAllDonnationsVille(int idVille);
+/*
+    List<Donnation> getAllDonnationsDonnateur(int idDonnateur);
+
+    int countDonnations(int villeFilter, int groupeFilter);
+    int countDonnationsPerCenter(int idCentre);
+
+*/
 }
