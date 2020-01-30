1987372ff8f65a96c337e8e15ddfc8b2e11be409
OsmMossati <56207208+OsmMossati@users.noreply.github.com>
2020-01-30 01:58:06 +0100

    Initializing my Controllers files

--- src/main/java/Controller/DonnationController.java
+++ src/main/java/Controller/DonnationController.java
@@ -1,4 +1,73 @@
 package Controller;
 
-public class DonnationController {
+import DAO.DAOFactory;
+import DAO.interfaces.*;
+import Model.*;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet("/Donnation")
+public class DonnationController extends HttpServlet {
+    private DAOFactory daoFactory = new DAOFactory("jdbc:mysql://localhost:3306/sang","root","");
+    private HttpSession httpSession;
+
+    private DonnationDAO donnationDAO;
+    private DonnateurDAO donnateurDAO;
+    private VilleDAO villeDAO;
+    private GroupeSanginDAO groupeSanginDAO;
+    private BanqueSangDAO banqueSangDAO;
+
+    @Override
+    public void init() throws ServletException {
+
+        daoFactory.getInstance();
+
+        donnationDAO = daoFactory.getDonnationDaoImpl();
+        donnateurDAO = daoFactory.getDonnateurDaoImpl();
+        villeDAO = daoFactory.getVilleDaoImpl();
+        groupeSanginDAO = daoFactory.getGroupeSanginDaoImpl();
+        banqueSangDAO = daoFactory.getBanqueSangDaoImpl();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        try {
+            showDonnations(request, response);
+
+        } catch (SQLException ex) {
+            throw new ServletException(ex);
+        }
+    }
+
+    private void showDonnations(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException, ServletException{
+
+        List<Donnation> donnationList = donnationDAO.getAllDennation();
+        List<Ville> villes = villeDAO.getAllVille();
+        List<Donnateur> donnateurs = donnateurDAO.getAllDonnateurs();
+        List<GroupeSangin> gsList = groupeSanginDAO.findAll();
+        List<BanqueSang> banqueSangList = banqueSangDAO.findAllBanqueSang();
+
+        request.setAttribute("villes",villes);
+        request.setAttribute("donnateurs",donnateurs);
+        request.setAttribute("donnationList",donnationList);
+        request.setAttribute("gsList",gsList);
+        request.setAttribute("banqueSangList",banqueSangList);
+
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/jsp/listDonnation.jsp");
+        requestDispatcher.forward(request,response);
+    }
 }
--- web/jsp/listDonnation.jsp
+++ web/jsp/listDonnation.jsp
@@ -1,16 +1,45 @@
 <%--
   Created by IntelliJ IDEA.
-  User: dell
-  Date: 29/01/2020
-  Time: 19:12
+  User: Mossati-Oussama
+  Date: 24/01/2020
+  Time: 02:12
   To change this template use File | Settings | File Templates.
 --%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
 <html>
 <head>
-    <title>$Title$</title>
+    <title>DONNATION</title>
+    <link rel="stylesheet" href="../frameworks/bootstap4/dist/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="../frameworks/font-awesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="../frameworks/font-awesome/css/regular.min.css">
+    <link rel="stylesheet" href="../frameworks/font-awesome/css/solid.min.css">
 </head>
+
 <body>
-$END$
+
+<header>
+    <%@ include file="navbar.jsp"%>
+</header>
+<br>
+
+
 </body>
+
+<script src="../frameworks/jquery/jquery.js"></script>
+<script src="../frameworks/bootstap4/dist/js/bootstrap.bundle.min.js"></script>
+<script>
+    $(function(){
+        $('[data-toggle="tooltip"]').tooltip();
+        $('#banque').on("keyup",function(){
+            var value=$(this).val().toLowerCase();
+            $('#bnq tr').filter(function(){
+                $(this).toggle($(this).text().toLowerCase().indexOf(value)>-1);
+            });
+        });
+    });
+</script>
+
 </html>
