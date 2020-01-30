ef0e2acbe6c95304716bf6d9e99c1555fd31ce23
ZOUHAIR <elmahdi2zouhair@gmail.com>
2020-01-30 20:23:24 +0100

    Start developing CRUD for Planning

---
+++ src/main/java/Controller/Planning/SupprimerPlanningServlet.java
@@ -0,0 +1,19 @@
+package Controller.Planning;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "SupprimerPlanningServlet")
+public class SupprimerPlanningServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
